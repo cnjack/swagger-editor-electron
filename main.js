afb60ab6460f891e4c1c5ffd4df2dc4e6c092dc4
jack <jack.wang@hp.com>
2020-05-19 15:17:24 +0800

    feat: remember the size info

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 const { app, BrowserWindow } = require('electron')
+const windowStateKeeper = require('electron-window-state');
 
 function createWindow () {
   // Create the browser window.
+  const mainWindowState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800
+  });
   let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    'x': mainWindowState.x,
+    'y': mainWindowState.y,
+    'width': mainWindowState.width,
+    'height': mainWindowState.height,
     webPreferences: {
       nodeIntegration: true
     }
   })
-
+  mainWindowState.manage(win);
   // and load the index.html of the app.
   win.loadFile('dist/index.html')
 }
